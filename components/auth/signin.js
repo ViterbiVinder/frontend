9b0464e8ae7c9b12d75deec2a03f46815798a363
Max Leiter <maxwell.leiter@gmail.com>
2020-04-25 20:34:17 -0700

    Added prettier pre-commit hook, removed some console.logs

--- components/auth/signin.js
+++ components/auth/signin.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from 'next/link'
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Router from 'next/router';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "next/link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Router from "next/router";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.primary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -42,44 +42,42 @@ export default function SignIn() {
   const [error, setError] = React.useState("");
 
   const signIn = async (e) => {
-
     e.preventDefault();
 
     const body = {
       username,
-      password
-    }
-
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body)
+      password,
     };
 
-    console.log(requestOptions)
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    };
 
-    await fetch('/api/signin', requestOptions)
-        .then(async response => {
-          const json = await response.json()
-          console.log(json.body)
-          if (json.body && json.body.Error) {
-            setError(json.body.Error)
-            return
-          } else {
-             localStorage.setItem("vinder-auth", JSON.stringify(true));
-             localStorage.setItem("vinder-name", JSON.stringify(json.body.name));
-             localStorage.setItem("vinder-username", JSON.stringify(json.body.username));
-             Router.push('/')
-          }
-        })
-  }
+    await fetch("/api/signin", requestOptions).then(async (response) => {
+      const json = await response.json();
+      if (json.body && json.body.Error) {
+        setError(json.body.Error);
+        return;
+      } else {
+        localStorage.setItem("vinder-auth", JSON.stringify(true));
+        localStorage.setItem("vinder-name", JSON.stringify(json.body.name));
+        localStorage.setItem(
+          "vinder-username",
+          JSON.stringify(json.body.username)
+        );
+        Router.push("/");
+      }
+    });
+  };
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon color="secondary"/>
+          <LockOutlinedIcon color="secondary" />
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
@@ -97,7 +95,9 @@ export default function SignIn() {
             autoComplete="user"
             autoFocus
             value={username}
-            onChange={(e) => { setUsername(e.target.value) }}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
           />
           <TextField
             variant="outlined"
@@ -110,7 +110,9 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
             value={password}
-            onChange={(e) => { setPassword(e.target.value) }}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import App from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../components/theme';
-import Drawer from '../components/drawer';
-import withAuth from '../utils/withAuth';
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../components/theme";
+import Drawer from "../components/drawer";
+import withAuth from "../utils/withAuth";
 
 class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -18,12 +18,14 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps, isAuthenticated } = this.props;
-    console.log("isAuthenticated? " + isAuthenticated)
     return (
       <React.Fragment>
         <Head>
           <title>Vinder</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
         </Head>
         <ThemeProvider theme={theme}>
           <CssBaseline />
@@ -35,5 +37,4 @@ class MyApp extends App {
   }
 }
 
-
-export default withAuth(MyApp)
+export default withAuth(MyApp);
