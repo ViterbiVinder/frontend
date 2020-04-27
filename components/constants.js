eaf285ed7c45951dd5297fb3a07ac228acfc31f9
Max Leiter <maxwell.leiter@gmail.com>
2020-04-26 22:27:31 -0700

    final!!

--- components/constants.js
+++ components/constants.js
@@ -1,4 +1,7 @@
 //export const API = "http://174.138.59.149:8080/VinderServer/";
 //export const DEPLOYMENT = "https://vinder.hacksc.now.sh/";
+
+// swap with above for test environment
+
 export const API = "http://127.0.0.1:8080/VinderServer/";
 export const DEPLOYMENT = "http://localhost:3000/";
