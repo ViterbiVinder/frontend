0eb9cd0a7ef08f257d0ef61a8e531e7115447362
Andreas Bigger <bigger@usc.edu>
2020-04-26 22:25:55 -0700

    patch running locally

--- components/constants.js
+++ components/constants.js
@@ -1,2 +1,4 @@
-export const API = "http://174.138.59.149:8080/VinderServer/";
-export const DEPLOYMENT = "https://vinder.hacksc.now.sh/";
+//export const API = "http://174.138.59.149:8080/VinderServer/";
+//export const DEPLOYMENT = "https://vinder.hacksc.now.sh/";
+export const API = "http://127.0.0.1:8080/VinderServer/";
+export const DEPLOYMENT = "http://localhost:3000/";
